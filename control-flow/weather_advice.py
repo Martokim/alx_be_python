b909b065eaa9c795e36b02f32c2e57d698a2e386
Marto <martinkimani42@gmail.com>
2025-05-28 12:21:38 +0300

    add weather_advice.py for conditional statement task

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -7,9 +7,3 @@ elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print(" Sorry, I don't have recommendations for this weather.")
-    
-    
-    
-    #     if weather = "sunny":
-#     print(" Wear a t-shirt and sunglasses.")
-#   elif weather =  "rainy":
