43c6e8c599bd1a59d8d86f21c5af4031a10775ba
Marto <martinkimani42@gmail.com>
2025-06-05 18:40:58 +0300

    Remove duplicate control-flow and python_introduction folders

--- python_introduction/basic_operations.py
+++
@@ -1,9 +0,0 @@
-number1 = 10
-number2 = 5
-sum = number1 + number2
-sub = number2 - number1
-mult = number1 * number2
-print('addition of ' , number1 ,'and' , number2 ,'is', sum )
-print('subtraction of ' , number2 ,'and' , number1, 'is' , sub)
-print('Multiplication of ', number1,'and' ,number2 , 'is' , mult)
-
