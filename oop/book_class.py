aa9a6c6f49e5a127848d41550aa62664ec8c7056
Marto <martinkimani42@gmail.com>
2025-06-21 19:39:02 +0300

    Task0:made some changes

--- oop/book_class.py
+++ oop/book_class.py
@@ -3,7 +3,9 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-
+    def __del__(self):
+        print(f"Deleting {self.title}")
+        
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
 
