93fea4e6c4805e642d4086fc0cfc36885d8abf30
Marto <martinkimani42@gmail.com>
2025-06-21 19:22:20 +0300

    Task 1: made some changes

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,17 +3,23 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        return f"{self.title} by {self.author}" 
+        return f"Book: {self.title} by {self.author}" 
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author) # initialize the base class usng super()
         self.file_size = file_size
 
+        def __str__(self):
+            return f"EBook: {self.title} by {self.author}, Size: {self.file_size}KB"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author) # initialize the base class using super()
         self.page_count = page_count
+    
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Pages: {self.page_count}"
 
 class Library:
     def __init__(self):
