21285027b3e79b9f95ed38043e74d0cf5205a958
Marto <martinkimani42@gmail.com>
2025-06-28 13:55:59 +0300

    Fix SQL syntax for bookstore DB

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -1,25 +1,39 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
 CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
-    author_id(foreign key reference Authors.author_id),
-    price DOUBLE PRECISION,
-    publication_date DATE
-)
-CREATE TABLE Authors (
-    author_id INT PRIMARY KEY,
-    author_name VARCHAR(215),
-)
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
 
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
-    adddress TEXT
-)
+    address TEXT
+);
 
-CREATE TABLE Order_details(
-    orderdetail_id INT PRIMARY KEY,
-    order_id(foreign key reference Orders.order_id),
-    book_id(foreign key reference Books.book_id),
-    quantity DOUBLE PRECISION,
-)
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
